adb694a8d54a058b5279706fa5e0eef117fdbd87
Soo-young Hwang <sooyoung971229@gmail.com>
2020-11-23 16:08:44 +0900

    Add batch size

--- intern-consumer-agent/src/main/java/com/humuson/agent/config/KafkaReceiveConfig.java
+++ intern-consumer-agent/src/main/java/com/humuson/agent/config/KafkaReceiveConfig.java
@@ -33,6 +33,14 @@ public class KafkaReceiveConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumer_groupid);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10000);
+        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 22 * 1024 * 1024);
+        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 50 * 1024 * 1024);
+        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 500);
+        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 50 * 1024 * 1024);
+
         return props;
     }
 
@@ -45,6 +53,7 @@ public class KafkaReceiveConfig {
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setBatchListener(true);
         return factory;
     }
 
