22e5e3e3a6e11ec84e5a8c8a05616d597c73abda
binsin <binyong318@gmail.com>
2020-12-15 11:45:53 +0900

    필요없는 것들 제거

--- intern-kafka-api/src/main/java/com/humuson/api/APIApplication.java
+++ intern-kafka-api/src/main/java/com/humuson/api/APIApplication.java
@@ -3,10 +3,8 @@ package com.humuson.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @PropertySource("classpath:kafka.properties")
-@EnableScheduling
 @SpringBootApplication
 public class APIApplication {
     public static void main(String[] args) {
